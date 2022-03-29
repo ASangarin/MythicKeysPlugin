d9f399f76ca72fb8fc12a862e658d118dc80acbb
ASangarin <ariasangarin@gmail.com>
2022-03-29 06:18:27 +0200

    support for latest mythicmobs api

--- src/main/java/eu/asangarin/mythickeys/compat/MythicMobsCompat.java
+++ src/main/java/eu/asangarin/mythickeys/compat/MythicMobsCompat.java
@@ -1,11 +1,12 @@
 package eu.asangarin.mythickeys.compat;
 
-import io.lumine.xikage.mythicmobs.MythicMobs;
-import io.lumine.xikage.mythicmobs.adapters.AbstractPlayer;
-import io.lumine.xikage.mythicmobs.adapters.bukkit.BukkitAdapter;
-import io.lumine.xikage.mythicmobs.mobs.GenericCaster;
-import io.lumine.xikage.mythicmobs.skills.SkillMetadata;
-import io.lumine.xikage.mythicmobs.skills.SkillTrigger;
+import io.lumine.mythic.api.adapters.AbstractPlayer;
+import io.lumine.mythic.api.mobs.GenericCaster;
+import io.lumine.mythic.api.skills.SkillMetadata;
+import io.lumine.mythic.bukkit.BukkitAdapter;
+import io.lumine.mythic.bukkit.MythicBukkit;
+import io.lumine.mythic.core.skills.SkillMetadataImpl;
+import io.lumine.mythic.core.skills.SkillTriggers;
 import org.bukkit.entity.Player;
 
 import java.util.HashSet;
@@ -14,12 +15,12 @@ public class MythicMobsCompat {
 	public static void runSkill(String id, Player player) {
 		if (id == null || id.isEmpty()) return;
 
-		MythicMobs.inst().getSkillManager().getSkill(id).ifPresent(skill -> {
+		MythicBukkit.inst().getSkillManager().getSkill(id).ifPresent(skill -> {
 			AbstractPlayer trigger = BukkitAdapter.adapt(player);
 			GenericCaster genericCaster = new GenericCaster(trigger);
-			SkillMetadata skillMeta = new SkillMetadata(SkillTrigger.API, genericCaster, trigger, BukkitAdapter.adapt(player.getLocation()),
+			SkillMetadata skillMeta = new SkillMetadataImpl(SkillTriggers.API, genericCaster, trigger, BukkitAdapter.adapt(player.getLocation()),
 					new HashSet<>(), null, 1.0F);
-			if (skill.usable(skillMeta, SkillTrigger.API)) skill.execute(skillMeta);
+			if (skill.isUsable(skillMeta, SkillTriggers.API)) skill.execute(skillMeta);
 		});
 	}
 }
